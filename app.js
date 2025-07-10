747e65a00835c93b01cb844f76c6beb41c2a4549
tommy tom <t.stear@gmail.com>
2025-07-10 14:12:44 -0500

    Adding logic to a user registration endpoint

--- app.js
+++ app.js
@@ -1,11 +1,56 @@
-require("dotenv").config();
-require("./config/database").connect();
-const express = require("express");
+require('dotenv').config();
+require('./config/database').connect();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const User = require('./model/user');
+const req = require('express/lib/request');
 
 const app = express();
 
 app.use(express.json());
 
-// Logic goes here
+// Register
+app.post('/register', async (req, res) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
+    if (!(firstName && lastName && email && password)) {
+      return res.status(400).send('Failed to register user.  Try again later.');
+    }
+
+    const oldUser = await User.findOne({ email });
+    if (oldUser) {
+      return res.status(409).send('User Already Exists.  Please Login.');
+    }
+
+    encryptedUserPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      first_name: firstName,
+      last_name: lastName,
+      email: email.toLowerCase(),
+      password: encryptedUserPassword,
+    });
+
+    const token = jwt.sign(
+      { user_id: user._id, email },
+      process.env.TOKEN_KEY,
+      {
+        expiresIn: '15m',
+      }
+    );
+    // save user token
+    user.token = token;
+
+    console.log(`User created:  [${firstName} ${lastName} ${email}]`);
+
+    res.status(201).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Login
+app.post('/login', (req, res) => {});
 
 module.exports = app;
