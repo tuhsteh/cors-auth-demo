5c7f4e3d9962a39476eb9972e5aba887d35f1a15
tommy tom <t.stear@gmail.com>
2025-07-11 12:11:02 -0500

    Adding Stylistic to replace deprecated ESLint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,22 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
+// import js from '@eslint/js';
+// import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+// import { FlatCompat } from 'eslint/config';
 
-export default defineConfig([
+export default [
   {
-    files: ['**/*.{js,mjs,cjs}'],
-    plugins: { js },
-    extends: ['js/recommended'],
+    // files: ['**/*.{js,mjs,cjs}'],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    // extends: ['js/recommended'],
+    rules: {
+      '@stylistic/eol-last': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/jsx-indent': 'error',
+      '@stylistic/semi': 'error',
+    },
   },
-  { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
-]);
+  // { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
+  // { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
+];
