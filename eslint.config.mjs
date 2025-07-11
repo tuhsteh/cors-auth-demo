df0447751a73cc3e220a16c46c93c816c276359c
tommy tom <t.stear@gmail.com>
2025-07-11 12:14:41 -0500

    Removing commented deprecated lines after making sure they are free to leave

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,10 @@
-// import js from '@eslint/js';
-// import globals from 'globals';
 import stylistic from '@stylistic/eslint-plugin';
-// import { FlatCompat } from 'eslint/config';
 
 export default [
   {
-    // files: ['**/*.{js,mjs,cjs}'],
     plugins: {
       '@stylistic': stylistic,
     },
-    // extends: ['js/recommended'],
     rules: {
       '@stylistic/eol-last': ['error', 'always'],
       '@stylistic/indent': ['error', 2],
@@ -17,6 +12,4 @@ export default [
       '@stylistic/semi': 'error',
     },
   },
-  // { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
-  // { files: ['**/*.{js,mjs,cjs}'], languageOptions: { globals: globals.node } },
 ];
