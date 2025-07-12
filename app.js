5ec4f06966aae1cb82033e91b79f9a47f0e8f56e
tommy tom <t.stear@gmail.com>
2025-07-11 20:15:42 -0500

    Method to login; method to verifyToken and to use it with a welcome endpoint;

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 require('@dotenvx/dotenvx').config();
 require('./config/database').connect();
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+
+const auth = require('./middleware/auth');
 const User = require('./model/user');
 //const req = require('express/lib/request');
 
@@ -51,6 +53,40 @@ app.post('/register', async (req, res) => {
 });
 
 // Login
-// app.post('/login', (req, res) => {});
+app.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    if (!(email && password)) {
+      return res.status(400).send('Failed to login user.  Try again later.');
+    }
+
+    const user = await User.findOne({ email });
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: '15m',
+        }
+      );
+      // save user token
+      user.token = token;
+
+      console.log(`User logged in:  [${user.first_name} ${user.last_name} ${email}]`);
+
+      return res.status(200).json(user);
+    } else {
+      res.status(400).send('Failed to login user.  Try again later.');
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// Welcome
+app.post('/welcome', auth, (req, res) => {
+  res.status(200).send(`${new Date()} Welcome to FreeCodeCamp 🙌`);
+});
 
 module.exports = app;
